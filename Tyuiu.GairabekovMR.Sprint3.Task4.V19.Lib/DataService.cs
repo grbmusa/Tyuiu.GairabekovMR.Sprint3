d2d158fe67717120584d0116f7d74b88613127f3
TyuiuGairabekovMR@outlook.com <TyuiuGairabekovMR@outlook.com>
2024-12-11 20:49:20 +0500

    Task5

--- Tyuiu.GairabekovMR.Sprint3.Task4.V19.Lib/DataService.cs
+++ Tyuiu.GairabekovMR.Sprint3.Task4.V19.Lib/DataService.cs
@@ -1,25 +1,31 @@
-﻿using tyuiu.cources.programming.interfaces.Sprint3;
+﻿using System;
+using tyuiu.cources.programming.interfaces.Sprint3;
 
-namespace Tyuiu.GairabekovMR.Sprint3.Task4.V19.Lib
+namespace Tyuiu.GairabekovMR.Sprint3.Task5.V3.Lib
 {
-    public class DataService : ISprint3Task4V19
-
+    public class DataService : ISprint3Task5V3
     {
         public double Calculate(int startValue, int stopValue)
         {
-            double res = 1;
-            for (int x = startValue; x <= stopValue; x++)
+            double result = 0;
+            for (int x = startValue; x >= stopValue; x--)
             {
-                if (x == 0)
+                result += Math.Pow(x, 2) / Math.Sin(x);
+            }
+            return Math.Round(result, 3);
+        }
+
+        public double GetSumSumSeries(int a, int b, int c, int d, int x)
+        {
+            double result = 0;
+            for (int i = a; i <= b; i++)
+            {
+                for (int k = c; k <= d; k++)
                 {
-                    break;
-                }
-                else
-                {
-                    res = res * (x / (Math.Cos(x) + x) + 0.5);
+                    result += (Math.Sin(k) + x) / (double)x;
                 }
             }
-            return Math.Round(res, 3);
+            return Math.Round(result, 3);
         }
     }
 }
