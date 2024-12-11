077d3dd5f2e1572028ad045b798126ffb2111455
TyuiuGairabekovMR@outlook.com <TyuiuGairabekovMR@outlook.com>
2024-12-11 17:53:31 +0500

    Task5

--- Tyuiu.GairabekovMR.Sprint3.Task5.V3.Lib/DataService.cs
+++ Tyuiu.GairabekovMR.Sprint3.Task5.V3.Lib/DataService.cs
@@ -3,9 +3,9 @@ using tyuiu.cources.programming.interfaces.Sprint3;
 
 namespace Tyuiu.GairabekovMR.Sprint3.Task5.V3.Lib
 {
-    
-    
-    public class DataService : ISprint3Task4V19
+
+
+    public class DataService : ISprint3Task5V3
     {
         public double Calculate(int startValue, int stopValue)
         {
