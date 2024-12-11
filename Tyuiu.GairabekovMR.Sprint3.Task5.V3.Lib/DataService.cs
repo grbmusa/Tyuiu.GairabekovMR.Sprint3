1b99db0294e3a695088eac2af207e3069181a000
TyuiuGairabekovMR@outlook.com <TyuiuGairabekovMR@outlook.com>
2024-12-11 20:58:44 +0500

    Task5

--- Tyuiu.GairabekovMR.Sprint3.Task5.V3.Lib/DataService.cs
+++ Tyuiu.GairabekovMR.Sprint3.Task5.V3.Lib/DataService.cs
@@ -3,7 +3,7 @@ using tyuiu.cources.programming.interfaces.Sprint3;
 
 namespace Tyuiu.GairabekovMR.Sprint3.Task5.V3.Lib
 {
-    public class DataService : ISprint3Task5V0
+    public class DataService : ISprint3Task5V3
     {
         public double GetSumSumSeries(int a, int b, int c, int d, int x)
         {
