a7f91e5d4a7879d22da6be8a2c776f43871c646f
TyuiuGairabekovMR@outlook.com <TyuiuGairabekovMR@outlook.com>
2024-12-12 14:09:43 +0500

    Task7

---
+++ Tyuiu.GairabekovMR.Sprint3.Task7.V18.Lib/DataSerice.cs
@@ -0,0 +1,30 @@
+﻿using System.Runtime.ExceptionServices;
+using tyuiu.cources.programming.interfaces.Sprint3;
+
+namespace Tyuiu.GairabekovMR.Sprint3.Task7.V18.Lib
+{
+    public class DataService : ISprint3Task7V18
+    {
+        public double[] GetMassFunction(int startValue, int stopValue)
+        {
+            double[] a = new double[stopValue - startValue + 1];
+            int i = 0;
+
+            for (int j = startValue; j <= stopValue; j++)
+            {
+                if ((Math.Cos(j) + j) == 0)
+                {
+                    a[i] = 0.0;
+                    i++;
+                }
+                else
+                {
+                    a[i] = Math.Round(((2 * j - 3) / (Math.Cos(j) + j)) + 5, 2);
+                    i++;
+                }
+            }
+
+            return a;
+        }
+    }
+}
---
+++ Tyuiu.GairabekovMR.Sprint3.Task7.V18/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Tyuiu.GairabekovMR.Sprint3.Task7.V18
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
